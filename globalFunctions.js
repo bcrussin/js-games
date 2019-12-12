af8fc55c3e053f36d55576b98ba98dc1c20a3401
bcrussin <cvillebear@gmail.com>
2019-12-12 09:26:30 -0500

    Initial commit

---
+++ globalFunctions.js
@@ -0,0 +1,153 @@
+//jshint maxerr: 999
+
+var key = {
+	pressed: {},
+	held: {},
+	noPress: {},
+	timers: {},
+	hasCooldowns: typeof cooldowns !== 'undefined',
+	
+	press: function(keyName) {
+		if(!this.noPress.hasOwnProperty(keyName)) {
+			this.pressed[keyName] = true;
+			this.held[keyName] = true;
+			//console.log(this.pressed);
+			if(this.hasCooldowns) this.timers[keyName] = cooldowns[keyName];
+		}
+	},
+	
+	release: function(keyName) {
+		delete this.pressed[keyName];
+		delete this.held[keyName];
+	},
+	
+	update: function() {
+		for(var i in this.timers) {
+			if(this.hasCooldowns && cooldowns[i] > 0) {
+				if(this.timers[i] === cooldowns[i] && !this.noPress.hasOwnProperty(i)) {
+					this.noPress[i] = true;
+					delete this.pressed[i];
+				}
+				this.timers[i]--;
+				if(this.timers[i] < 0) {
+					delete this.noPress[i];
+					if(this.held[i]) this.pressed[i] = true;
+				}
+			}
+		}
+		
+		//console.log(this.pressed);
+	},
+	
+	get: function(keyName) {
+		return this.pressed.hasOwnProperty(keyName);
+	}
+};
+
+var c = {
+	rect: function(x, y, w, h, c, alpha) {
+		if(isNaN(alpha)) ctx.globalAlpha = 1;
+		else ctx.globalAlpha = alpha;
+		ctx.fillStyle = c;
+		ctx.fillRect(x, y, w, h);
+	},
+		
+	ellipse: function(x, y, w, h, c, rot, startAngle, endAngle, switchDir, alpha) {
+		if(isNaN(alpha)) ctx.globalAlpha = 1;
+		else ctx.globalAlpha = alpha;
+		rot = rot || 0;
+		startAngle = startAngle || 0;
+		endAngle = endAngle || Math.PI * 2;
+		if(isNaN(switchDir)) switchDir = false;
+		ctx.fillStyle = c;
+		ctx.beginPath();
+		ctx.ellipse(x, y, w, h, rot, startAngle, endAngle, switchDir);
+		ctx.fill();
+	},
+	
+	stroke: {
+		rect: function(x, y, w, h, c, alpha) {
+			if(isNaN(alpha)) ctx.globalAlpha = 1;
+			else ctx.globalAlpha = alpha;
+			ctx.strokeStyle = c;
+			ctx.strokeRect(x, y, w, h);
+		},
+		
+		ellipse: function(x, y, w, h, t, c, rot, startAngle, endAngle, switchDir, alpha) {
+			if(isNaN(alpha)) ctx.globalAlpha = 1;
+			else ctx.globalAlpha = alpha;
+			rot = rot || 0;
+			startAngle = startAngle || 0;
+			endAngle = endAngle || Math.PI * 2;
+			if(isNaN(switchDir)) switchDir = false;
+			ctx.strokeStyle = c;
+			ctx.lineWidth = t;
+			ctx.beginPath();
+			ctx.ellipse(x, y, w, h, rot, startAngle, endAngle, switchDir);
+			ctx.stroke();
+		}
+	},
+  
+	line: function(x1, y1, x2, y2, thickness, c, alpha) {
+		if(isNaN(alpha)) ctx.globalAlpha = 1;
+		else ctx.globalAlpha = alpha;
+		ctx.strokeStyle = c;
+		ctx.beginPath();
+		ctx.lineWidth = thickness;
+		ctx.moveTo(x1, y1);
+		ctx.lineTo(x2, y2);
+		ctx.stroke();
+	},
+  
+	text: function(text, x, y, size, c, centered, alpha) {
+		if(isNaN(alpha)) ctx.globalAlpha = 1;
+		else ctx.globalAlpha = alpha;
+		if(centered === true) ctx.textAlign = "center";
+		else ctx.textAlign = "left";
+		ctx.font = size + "px Arial";
+		ctx.fillStyle = c;
+		ctx.textAlign = centered;
+		ctx.fillText(text, x, y);
+	},
+	
+	img: function(name, x, y, w, h, rot, centered) {
+		if(images.loaded) {
+			rot = rot || 0;
+			centered = centered || false;
+			let offsetX = centered ? (w / 2) : 0;
+			let offsetY = centered ? (h / 2) : 0;
+			let image = images.data[name];
+			ctx.save();
+			ctx.translate(x, y);
+			ctx.rotate(rot * Math.PI / 180);
+			ctx.drawImage(images.data[name], 0 - offsetX, 0 - offsetY, w, h);
+			ctx.restore();
+		}
+	},
+	
+	clear: function() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	}
+};
+
+
+//_____ NUMBER FUNCTIONS _____//
+
+function randomf(min, max) {
+	return (Math.random() * (max - min)) + min;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a === null || b === null) return false;
+  if (a.length != b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
---
+++ index.js
@@ -0,0 +1,187 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var menu = {
+  x: -1000,
+  index: 0,
+  spacing: 350,
+  speed: 10,
+  dir: null,
+  dest: null,
+  selected: false,
+  
+  select: function() {
+    var loc = window.location.pathname;
+    this.dir = loc.substring(0, loc.lastIndexOf('/'));
+    this.dest = imageData[menu.index].dest;
+    this.selected = true;
+  }
+};
+
+//_____ IMAGE DATA _____//
+//var images = document.images;
+
+var imageData = [
+  {
+    name: "Tetris",
+    dest: "/Tetris/tetris.html",
+    img: "tetris"
+  },
+  {
+    name: "Flood",
+    dest: "/Flood/flood.html",
+    img: "flood"
+  },
+  {
+  	name: "Snake",
+  	dest: "/Snake/snake.html",
+  	img: "snake"
+  },
+  {
+    name: "Knockoff",
+    dest: "/Knockoff/knockoff.html",
+    img: "knockoff"
+  },
+  {
+    name: "Tron",
+    dest: "/Tron/tron.html",
+    img: "tron"
+  },
+  {
+    name: "Raycast",
+    dest: "/Raycast/raycast.html",
+    img: "raycast"
+  },
+  {
+    name: "Map Maker",
+    dest: "/Map Maker/map_maker.html",
+    img: "map_maker"
+  },
+  {
+    name: "Asteroids",
+    dest: "/Asteroids/asteroids.html",
+    img: "asteroids"
+  },
+  {
+  	name: "Auto Sim",
+  	dest: "/Auto Sim/auto_sim.html",
+  	img: "auto_sim"
+  },
+  {
+  	name: "Survival",
+  	dest: "/Survival/survival.html",
+  	img: "survival"
+  }
+];
+
+var images = [];
+for(let i = 0; i < imageData.length; i++) {
+	images[i] = document.getElementById(imageData[i].img);
+}
+
+//_____ RENDER FUNCTIONS _____//
+
+var c = {
+  rect: function(x, y, w, h, c) {
+    ctx.fillStyle = c;
+    ctx.fillRect(x, y, w, h);
+  },
+  
+  text: function(text, x, y, size, c, centered) {
+    if(centered === true) ctx.textAlign = "center";
+    else ctx.textAlign = "left";
+    ctx.font = size + "px Arial";
+    ctx.fillStyle = c;
+    ctx.textAlign = centered;
+    ctx.fillText(text, x, y);
+  }
+};
+
+//_____ KEYBOARD INPUT _____//
+
+var key = {
+  right: false,
+  left: false,
+  select: false
+};
+
+document.addEventListener('keydown', function(e) {
+  e.preventDefault();
+  switch(e.keyCode) {
+    case 39:
+      key.right = true;
+      if(menu.index < imageData.length - 1) menu.index++;
+      else menu.index = 0;
+      break;
+    case 37:
+      key.left = true;
+      if(menu.index > 0) menu.index--;
+      else menu.index = imageData.length - 1;
+      break;
+    case 32:
+      key.select = true;
+      menu.select();
+      break;
+  }
+});
+
+document.addEventListener('keyup', function(e) {
+  e.preventDefault();
+  switch(e.keyCode) {
+    case 39:
+      key.right = false;
+      break;
+    case 37:
+      key.left = false;
+      break;
+    case 32:
+      key.select = false;
+      break;
+  }
+});
+
+//_____ SETUP AND UPDATE FUNCTIONS _____//
+
+function setup() {
+  canvas.width = window.innerWidth - 10;
+  canvas.height = window.innerHeight - 25;
+  window.requestAnimationFrame(update);
+}
+
+function update() {
+  canvas.width = window.innerWidth - 10;
+  canvas.height = window.innerHeight - 25;
+  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  if(menu.selected) {
+    if(menu.x > -900) menu.x += (-1000 - menu.x) / menu.speed;
+    else window.location = menu.dir + menu.dest;
+  } else {
+    menu.x += ((menu.index * menu.spacing) - menu.x) / menu.speed;
+  }
+  
+  //c.text(menu.index, 5, 25, 20, "white");
+  
+  var left = Math.floor(((canvas.width / 2) / menu.spacing) - menu.index) - 1;
+  var right = Math.ceil(((canvas.width / 2) / menu.spacing) + menu.index);
+  
+  console.log(left + ", " + right);
+  
+  for(var i = Math.max(left, 0); i < Math.min(imageData.length, right); i++) {
+    var sizeMod = (Math.pow((i * menu.spacing) - menu.x, 2) / 100000) + 1.5;
+    sizeMod /= canvas.height / 500;
+    
+    var newWidth = images[i].width / sizeMod;
+    var newHeight = images[i].height / sizeMod;
+    
+    ctx.drawImage(images[i], ((canvas.width / 2) - (newWidth / 2)) + ((i * menu.spacing) - menu.x), canvas.height / 20, newWidth, newHeight);
+    c.text(imageData[i].name, ((canvas.width / 2) + ((i * menu.spacing) - menu.x)), ((images[i].height * 1.25) / sizeMod), 100 / sizeMod, "white", true);
+  }
+  
+  c.text("(use arrows + space to navigate)", canvas.width / 2, canvas.height - 10, 15, "white", true);
+  
+  window.requestAnimationFrame(update);
+}
+
+setup();
